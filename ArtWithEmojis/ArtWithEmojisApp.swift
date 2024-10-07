e713357902f34973b1bbe33b31a55ca6b7410f8f
Faheem Ahmed <faheemahmed5525@gmail.com>
2024-10-07 09:41:49 +0500

    Initial Commit

---
+++ ArtWithEmojis/ArtWithEmojisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtWithEmojisApp.swift
+//  ArtWithEmojis
+//
+//  Created by Faheeam Ahmed on 07/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ArtWithEmojisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
